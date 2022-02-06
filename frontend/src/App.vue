059d51d3a57de2b9623d2e249a6c9594e6081daa
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-02-06 21:01:51 +0500

    Finish Front Page (Vue)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
-    Hello world
+
+      <form @submit.prevent="submitForm">
+        <div class="row">
+          <div class="col-lg-3 my-3">
+            <input type="text" class="form-control" placeholder="Name" v-model="student.name">
+          </div>
+          <div class="col-lg-3 my-3">
+            <input type="text" class="form-control" placeholder="Course" v-model="student.course">
+          </div>
+          <div class="col-lg-3 my-3">
+            <input type="text" class="form-control" placeholder="Rating" v-model="student.rating">
+          </div>
+          <button class="btn btn-success col-lg-3 my-3">Submit</button>
+        </div>
+      </form>
+
+      <table class="table table-striped">
+        <thead>
+          <th>Name</th>
+          <th>Course</th>
+          <th>Rating</th>
+        </thead>
+        <tbody>
+          <tr v-for="student in students" :key="student.id" @dblclick="$data.student=student">
+            <td>{{ student.name }}</td>
+            <td>{{ student.course }}</td>
+            <td>{{ student.rating }}</td>
+            <td><button class="btn btn-outline-danger btn-sm mx-1" @click="deleteStudent(student)">x</button></td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
@@ -8,8 +38,69 @@
 
 export default {
   name: 'App',
-  components: {
-    
+  components: {},
+  data() {
+    return {
+      student: {},
+      students: []
+    }
+  },
+  async created() {
+    await this.getStudents()
+  },
+  methods: {
+    async getStudents() {
+      var response = await fetch('http://localhost:8000/api/students/')
+      this.students = await response.json()
+    },
+    submitForm () {
+      if (this.student.id === undefined) {
+        this.createStudent()
+      } else {
+        this.editStudent()
+      }
+    },
+    async createStudent() {
+      await this.getStudents()
+
+      await fetch('http://localhost:8000/api/students/', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          
+        },
+        body: JSON.stringify(this.student)
+      })
+      await this.getStudents()
+    },
+    async editStudent() {
+      await this.getStudents()
+
+      await fetch(`http://localhost:8000/api/students/${this.student.id}/`, {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json',
+          
+        },
+        body: JSON.stringify(this.student)
+      })
+      await this.getStudents()
+      
+      this.student = {}
+    },
+    async deleteStudent(student) {
+      await this.getStudents()
+
+      await fetch(`http://localhost:8000/api/students/${student.id}/`, {
+        method: 'delete',
+        headers: {
+          'Content-Type': 'application/json',
+          
+        },
+        body: JSON.stringify(this.student)
+      })
+      await this.getStudents()
+    }
   }
 }
 </script>
