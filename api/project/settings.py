8ae18b9c7a6b18560edbf45ea58d98d908ba82ac
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-02-07 16:45:53 +0500

    Heroku Deploy Success

--- api/project/settings.py
+++ api/project/settings.py
@@ -112,8 +112,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
 django_heroku.settings(locals())
 
 # Default primary key field type
